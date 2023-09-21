4cd945b89585dd7ba654d812d7cd6177e84230a6
Duberly, Guarnizo Fuentes Rivera <duberlygfr@gmail.com>
2023-09-21 13:44:50 -0500

    Initial Commit

---
+++ src/main/java/com/duberlyguarnizo/passiveoperationsservice/PassiveOperationsServiceApplication.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2023. Code by Duberly Guarnizo <duberlygfr@gmail.com>.
+ */
+
+package com.duberlyguarnizo.passiveoperationsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class PassiveOperationsServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PassiveOperationsServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/duberlyguarnizo/passiveoperationsservice/PassiveOperationsServiceApplicationTests.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2023. Code by Duberly Guarnizo <duberlygfr@gmail.com>.
+ */
+
+package com.duberlyguarnizo.passiveoperationsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PassiveOperationsServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
